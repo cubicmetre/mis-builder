7193aa9c2edcbe319ed935f43b0c3ccf5308aca9
FinalStaticFox <16882447+FinalStaticFox@users.noreply.github.com>
2026-01-02 13:33:25 +0100

    Added test coverage statistics

--- src/vite.env.d.ts
+++ src/vite.env.d.ts
@@ -1,3 +1,4 @@
+// Transform SVGs into React components by importing them with the "?react" suffix
 /// <reference types="vite-plugin-svgr/client" />
 
 // Recognize all CSS files as module imports.
